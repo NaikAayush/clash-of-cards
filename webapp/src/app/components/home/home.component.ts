176a6d44592c67135cbaffabdb6d4a7e7a563ef4
Samyak S Sarnayak <samyak201@gmail.com>
2022-03-13 18:51:19 +0530

    drag and stuff in home screen

--- webapp/src/app/components/home/home.component.ts
+++ webapp/src/app/components/home/home.component.ts
@@ -1,4 +1,12 @@
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { Card, CardMeta } from 'src/app/models/card';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +14,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  collection: Card[];
+  collectionDropList: Card[][];
+  selectedCardList: Card[][];
+  selectedZoneIds = Array.from(Array(10).keys()).map(
+    (i) => `selected-zone-${i}`
+  );
+  collectionZoneIds: string[];
+
+  constructor() {
+    // TODO: remove dis
+    const someCardMeta: CardMeta = {
+      imgUrl: '/assets/images/card-example.svg',
+      damage: 100,
+      maxHealth: 500,
+    };
+    this.collection = [
+      new Card(someCardMeta),
+      new Card(someCardMeta),
+      new Card(someCardMeta),
+      new Card(someCardMeta),
+      new Card(someCardMeta),
+      new Card(someCardMeta),
+      new Card(someCardMeta),
+      new Card(someCardMeta),
+    ];
+    this.collection.forEach((card) => {
+      card.added = true;
+    });
+
+    this.collectionDropList = this.collection.map((card) => [card]);
+    this.collectionZoneIds = Array.from(
+      Array(this.collectionDropList.length).keys()
+    ).map((i) => `collection-zone-${i}`);
+    this.selectedCardList = Array.from({ length: 8 }, () => []);
+  }
 
   ngOnInit(): void {}
+
+  dropCard(event: CdkDragDrop<Card[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      if (event.container.data.length == 0) {
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex
+        );
+      }
+    }
+
+    console.log(event.previousContainer.data, event.container.data);
+  }
+
+  canBeDropped(drag: CdkDrag, drop: CdkDropList) {
+    return drop.data.length === 0;
+  }
 }
