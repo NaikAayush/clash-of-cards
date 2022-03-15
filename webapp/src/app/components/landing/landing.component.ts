7fdf86732f073eb85c39ae2966d9a7ea32dc4eb4
NaikAayush <aayushnai@gmail.com>
2022-03-15 12:12:00 +0530

    landing changes

--- webapp/src/app/components/landing/landing.component.ts
+++ webapp/src/app/components/landing/landing.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent implements OnInit {
+  scaleSetting = {
+    scale: 1.1,
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
