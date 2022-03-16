f5d18e1da8bfdb3fb49caf3e8fdda09c3c98f758
Samyak S Sarnayak <samyak201@gmail.com>
2022-03-16 17:28:20 +0530

    udpate match contract address

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   clashTokenAddress: '0x31fF5086cb3d0c62f7049Ff0164b1a41e3C64627',
   clashNFTAddress: '0x8E4E2F6bf09fF635C6522b0633B03C7CB4548270',
-  clashMatchAddress: '0x07e16751083C639eB370f486bBe957A0E7CF19b7',
+  clashMatchAddress: '0x9703761fA48279Be766f1139D02ae0857d7D51f5',
   pinata_api_key: 'a9b72688ca05cbded4de',
   pinata_secret_api_key:
     '05e3dfe9712ad57c1b89e1dbb49e352adc31d772761b8327a866882884a1ec85',
