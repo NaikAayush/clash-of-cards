1b92363ca556cf9670863c54c90c3db901120f5e
NaikAayush <aayushnai@gmail.com>
2022-03-16 21:32:22 +0530

    final landing

--- webapp/src/app/components/landing/landing.component.ts
+++ webapp/src/app/components/landing/landing.component.ts
@@ -9,7 +9,15 @@ export class LandingComponent implements OnInit {
   scaleSetting = {
     scale: 1.1,
   };
+  scaleLogoSetting = {
+    scale: 1.1,
+    max: 0,
+  };
   constructor() {}
 
   ngOnInit(): void {}
+
+  goToLink(url: string) {
+    window.open(url, '_blank');
+  }
 }
